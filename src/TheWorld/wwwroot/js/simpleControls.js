b401cc5c2a403459ba29d6d68dadb5f6750d4340
John Tandy <john.tandy@barnabasfund.org>
2017-07-25 14:37:27 +0100

    ok

---
+++ src/TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,14 @@
+﻿//simpleControls.js
+(function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            restrict : "E",
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+})();
