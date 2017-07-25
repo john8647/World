f31949eaba2a4b9e80718de09985a6682baab5da
John Tandy <john.tandy@barnabasfund.org>
2017-07-25 15:07:54 +0100

    add routing from trips view page

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -2,6 +2,15 @@
 (function () {
     "use strict";
 
-    angular.module("app-trips", []);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/", {
+            controller: "tripsController",
+            controllerAs: "vm",
+            templateUrl: "/views/tripsView.html"
+        });
+
+        $routeProvider.otherwise({ redirectTo: "/"});
+    });
 
 })();
